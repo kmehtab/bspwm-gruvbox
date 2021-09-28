f1569590d88ad171cce62215fc284d84db4f70ba
kmehtab <kmehtab20@gmail.com>
2021-09-28 12:42:59 +0530

    made some small changes
    added gruvbox fg color in eww and rofi
    added button hover in eww

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -57,11 +57,11 @@ case $desktop in
     bspwm|/usr/share/xsessions/bspwm)
     if type "xrandr" > /dev/null; then
       for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-        MONITOR=$m polybar --reload mainbar-bspwm -c ~/.config/polybar/config.ini &
-        MONITOR=$m polybar --reload systray -c ~/.config/polybar/config.ini &
+        MONITOR=$m polybar --reload mainbar-bspwm -c ~/.config/polybar/config &
+        MONITOR=$m polybar --reload systray -c ~/.config/polybar/config. &
       done
     else
-    polybar --reload mainbar-bspwm -c ~/.config/polybar/config.ini &
+    polybar --reload mainbar-bspwm -c ~/.config/polybar/config. &
     fi
     # second polybar at bottom
     # if type "xrandr" > /dev/null; then
